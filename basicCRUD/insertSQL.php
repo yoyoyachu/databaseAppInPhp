39cd8153b22bd0d0deb937ce4edf4802e8504c2d
yoyoyachu <ryachna@gmail.com>
2021-05-18 00:02:14 -0400

    inserting data into database in PHP

---
+++ basicCRUD/insertSQL.php
@@ -0,0 +1,38 @@
+<h1>Enter Your Data Here...</h1>
+
+<?php 
+echo "<pre>\n";
+require_once "pdo.php";
+if(isset($_POST['name']) && isset($_POST['email']) && isset($_POST['password'])){
+    $sql = "INSERT INTO users(name,email,password) VALUES(:name,:email,:password)";
+    echo "<pre>\n".$sql."\n</pre>\n" ;
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute(array(
+        ':name' => $_POST['name'],
+        ':email' => $_POST['email'],
+        ':password' => $_POST['password']
+    ));
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="">
+    </head>
+    <body>
+    <form method= "post">
+        <p>Name: <input type="text" name="name" size="40"></p>
+        <p>Email: <input type="text" name="email"></p>
+        <p>Password: <input type="password" name="password"></p>
+        <p><input type="submit" value="Add New"></p>   
+    </form>
+
+    </body>
+</html>
+
