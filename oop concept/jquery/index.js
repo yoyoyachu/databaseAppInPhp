01c56818fb74c796b6c93b8dc705403d3ea72430
yoyoyachu <ryachna@gmail.com>
2021-07-21 13:04:40 -0400

    resizing width and height of window object

--- oop concept/jquery/index.js
+++ oop concept/jquery/index.js
@@ -1,4 +1,10 @@
-$(document).ready(function(){
-    alert('welcome to my page');
-    console.log('jquery demo')
+// $(document).ready(function(){
+//     alert('welcome to my page');
+//     console.log('jquery demo')
+// })
+
+
+$(window).resize(function(){
+    console.log(`resizing window with width = ${$(window).width()} & height = ${$(window).height()}`)
 })
+
