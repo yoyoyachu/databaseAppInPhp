1ec8cb64c01fcc1505f4afdb982b7ad133757566
yoyoyachu <ryachna@gmail.com>
2021-07-21 23:01:08 -0400

    ajax call to create chat box

---
+++ oop-concept/jquery/index.js
@@ -0,0 +1,10 @@
+// $(document).ready(function(){
+//     alert('welcome to my page');
+//     console.log('jquery demo')
+// })
+
+
+// $(window).resize(function(){
+//     console.log(`resizing window with width = ${$(window).width()} & height = ${$(window).height()}`)
+// })
+
